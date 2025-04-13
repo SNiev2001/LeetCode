b8e8e17cd5c0b93f6342d5b8edd356a06f1f76f0
SNiev <sergionievas01@gmail.com>
2025-04-13 17:58:23 -0500

    Create file

---
+++ 4-MedianofTwoSortedArrays/version/python3/4-MedianofTwoSortedArrays_20250413_175823.py
@@ -0,0 +1,31 @@
+# Last updated: 4/13/2025, 5:58:23 PM
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+
+        if not s:
+            return ""
+
+        def expandAroundCenter(left: int, right: int) -> str:
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left+1:right]
+
+        longest = ""
+        for i in range(len(s)):
+            # Odd length
+            odd_pal = expandAroundCenter(i, i)
+            if len(odd_pal) > len(longest):
+                longest = odd_pal
+
+            # Even length
+            even_pal = expandAroundCenter(i, i + 1)
+            if len(even_pal) > len(longest):
+                longest = even_pal
+
+        return longest
+        
+        return max(length)
+            
+
+        
