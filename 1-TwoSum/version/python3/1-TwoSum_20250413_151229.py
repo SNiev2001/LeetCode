8000bddc082f256955f04bd805637a26b1f11618
SNiev <sergionievas01@gmail.com>
2025-04-13 15:12:29 -0500

    Create file

---
+++ 1-TwoSum/version/python3/1-TwoSum_20250413_151229.py
@@ -0,0 +1,23 @@
+# Last updated: 4/13/2025, 3:12:29 PM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        list = ListNode()
+        head = list
+        carry = 0
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            list.next = ListNode(carry % 10)
+            list = list.next
+            carry //= 10
+        return head.next
+        
