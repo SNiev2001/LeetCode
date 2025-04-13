75b9eaec4a45fef38d574b98475ec705c3cc2e60
SNiev <sergionievas01@gmail.com>
2025-04-13 17:09:29 -0500

    Create file

---
+++ 1-TwoSum/version/python3/1-TwoSum_20250413_170929.py
@@ -0,0 +1,17 @@
+# Last updated: 4/13/2025, 5:09:29 PM
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = set()
+        left = 0
+        max_len = 0
+
+        for right in range(len(s)):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+            seen.add(s[right])
+            max_len = max(max_len, right - left + 1)
+
+        return max_len
+
+                
