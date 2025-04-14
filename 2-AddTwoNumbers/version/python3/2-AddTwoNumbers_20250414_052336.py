18d1d8ed5c0519256057dcae13e2784f587ea920
SNiev <sergionievas01@gmail.com>
2025-04-14 05:23:36 -0500

    Create file

---
+++ 2-AddTwoNumbers/version/python3/2-AddTwoNumbers_20250414_052336.py
@@ -0,0 +1,20 @@
+# Last updated: 4/14/2025, 5:23:36 AM
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        if x < 10 == 0:
+            return True
+        
+        arr = []
+        while x / 10 != 0:
+            arr.append(x%10)
+            x=x//10
+        n=len(arr)
+        for i in range(n//2):
+            print(arr)
+            print(arr[i],arr[n-1-i])
+            if arr[i] != arr[n-1-i]:
+                return False
+        return True
+        
