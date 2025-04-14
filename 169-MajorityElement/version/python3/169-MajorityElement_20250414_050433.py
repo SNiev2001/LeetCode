0a638ba27c19be60b7457752e1409d5964511dcc
SNiev <sergionievas01@gmail.com>
2025-04-14 05:04:34 -0500

    Create file

---
+++ 169-MajorityElement/version/python3/169-MajorityElement_20250414_050433.py
@@ -0,0 +1,19 @@
+# Last updated: 4/14/2025, 5:04:33 AM
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) <= 2:
+            return len(nums)
+
+        head = 2
+        for i in range(2, len(nums)):
+            if nums[i] != nums[head-2]:
+                nums[head] = nums[i]
+                head += 1
+
+        return head
+
+                
+
+            
+            
+        
